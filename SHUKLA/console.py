fd010c08fb73fdabc6a81e3dc3900c0f2c6471b1
It's RAJPUT <160611013+ITS-MY-EDIT@users.noreply.github.com>
2024-08-08 10:20:45 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SHUKLA/console.py
+++ SHUKLA/console.py
@@ -31,13 +31,13 @@ if os.path.exists("Internal"):
 API_ID = getenv("API_ID", "28795512")
 API_HASH = getenv("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 BOT_TOKEN = getenv("BOT_TOKEN", "6827959192:AAHUPK9y3_b73VMWEBb9G1fyLV--vLGwT9k")
-STRING_SESSION = getenv("STRING_SESSION", "BQG3YngAQZtDJAiytDIE2oGU0b7F3iIYSgDg-ShMKN_WtaWl9MX5e2io4Pj_0WBACx69MjPuKERcYHHQtaDg4g0uWQzkIGRpCdibH3hqgLrwXVcwEFGBOaMsyS2Jasop782UfMitqqtkfs_Y-KBN3uXebS7XxXI0t6XNueDqpWQCfCmvki5dCxbIXDXAEip_yXIrYYDssqeCPy1FeFjqEqLfCYtbtmQJeuxRp-p1xNBXzaYbp0e4KeOsgtPedDnf53Kfa1OUZVNSxMypqn6B67BMrVeZj_ul8rIYeMk1INWO1lQU35hz-ciD0SVtZ3HhzUd8QIk2D4ZBlkXHIFkRrnjNM9PqGwAAAAFjN393AA")
+STRING_SESSION = getenv("STRING_SESSION", "BQF6x0kAIPgI0PfO7RletTSfAKcrD1tNu24_fL3zljhvTCeWsZFvTIV7mV45BRd2MfVsVFL4Nuf8LXfxVuoT2C4q3-HLBkOy99uRIdRQ7MoIJhaPuZg4JvfmXQ4FYrEDJoK0D_bkPV23XcBLO3Y4NwcQPhexSIFviPoPMFW710EbX3JgYvC8jpCvptZ_W97PvaOuNM6sXHfsK-Ca97Ie6yPZzl5WS8ucUovw-_JW4WuKcxIMxvhpzpsxALa61ZGR-RSWXlBT_VCnFhwoW_NU1FSPDz_kfghJQdpl0nVn6qf0ZzMV9HMg6UPaccRVx32pw6SK8AiNcGXUvR-6Kp_8hcO1i7vSeAAAAAGDRWQgAA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://SachinSanatani:SACHINxSANATANI@sanatani.bnmsfbd.mongodb.net/?retryWrites=true&w=majority&appName=Sanatani")
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", "-1001970127211")
 OWNER_ID = getenv("OWNER_ID", "5959548791")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "V_VIP_OWNER")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5655278269").split()))
-ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6497330208").split()))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/4d753794650a16813b817.jpg")
 
 
 # OPTIONAL VARIABLES
@@ -55,7 +55,7 @@ PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
 
 
 # USERBOT DEFAULT IMAGE
-USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://graph.org/file/520cc1f61254a12050f2c.jpg")
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/4d753794650a16813b817.jpg")
 
 
 
